2451b581f3c7aea8d449142dd023abde0cd59482
jeroen van heiningen <jeroen.v.heiningen@gmail.com@>
2021-12-06 12:34:16 +0100

    cheated 6b :(

--- 06.py
+++ 06.py
@@ -1,43 +1,22 @@
-from dataclasses import dataclass
-from typing import List, Optional
+from typing import List
 from tools import InputProcessor
+from collections import deque
 
-@dataclass
-class Fish:
-    age: int
-
-
-class StateInputProcessor(InputProcessor):
+class StateInputProcessor(InputProcessor[List[int]]):
     def process_line(self, line: str) -> List[int]:
         return [int(n) for n in line.split(',')]
 
-    def getstate(self) -> List[Fish]:
-        numbers = self.process_input()[0]
-        return [Fish(age) for age in numbers]
 
+inital_state = StateInputProcessor('06.txt').process_input()[0]
 
+# ammount of fish per day old:
+#               0  1  2  3  4  5  6  7  8
+fishes = deque([0, 0, 0, 0, 0, 0, 0, 0, 0]) # rotatable list thingy
+for fish in inital_state:
+    fishes[fish] += 1
 
+for day in range(256):
+    fishes.rotate(-1) #  all parents have one child
+    fishes[6] += fishes[8] # all childs had one parent
 
-def create_baby_fish(mamma: Fish) -> Optional[Fish]:
-    if mamma.age == 0:
-        return Fish(age=8)
-    return None
-
-fishes = StateInputProcessor('06.txt').getstate()
-
-
-def solve(initial: List[Fish], days:int)-> List[Fish]:
-    fishes = initial
-    for day in range(days):
-        babies = []
-        for fish in fishes:
-            baby = create_baby_fish(mamma=fish)
-            if baby:
-                babies.append(baby)
-                fish.age =6
-                continue
-            fish.age -=1
-        fishes += babies
-    return fishes
-
-print(len(solve(fishes,80))) #359999
+print(sum(fishes))
