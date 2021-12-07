5087e39f46258dc9f828879324fa6c01d581796c
jeroen van heiningen <jeroen.v.heiningen@gmail.com@>
2021-12-07 07:47:55 +0100

    got 07b

--- 07.py
+++ 07.py
@@ -9,7 +9,10 @@ crab_posisions = CommaInputProcessor('07.txt').process_input()[0]
 def calulate_fuel(crab_posissions: List[int], pos: int) -> int:
     feul = 0
     for c in crab_posisions:
-        feul += abs(c - pos)
+        steps = abs(c - pos)
+        for i in range(1, steps+1):
+            feul += i
     return feul
 
-print(min([calulate_fuel(crab_posisions, pos) for pos in crab_posisions]))
+possible_pos = range(min(crab_posisions), max(crab_posisions)+1)
+print(min([calulate_fuel(crab_posisions, pos) for pos in possible_pos])) # 92881128
