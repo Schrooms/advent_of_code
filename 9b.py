41c4c6465ac7e2df2cb66c22c3d46a7d04eb88db
Jeroen van Heiningen <jvanheiningen@cloudsuite.com>
2021-12-10 11:26:32 +0100

    fixed it omg

--- 9b.py
+++ 9b.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Set, Tuple
 from typing_extensions import Literal
 from tools import InputProcessor
 from dataclasses import dataclass
@@ -9,13 +9,13 @@ class IntProcessor(InputProcessor[List[int]]):
         return [int(x) for x in line.strip('\n')]
 
 
-@dataclass
+@dataclass(frozen=True)
 class Location:
     value: int
     row: int
     column: int
 
-map = IntProcessor('09b.txt').process_input()
+map = IntProcessor('09.txt').process_input()
 
 def get_low_points(height_map:List[List[int]]) -> List[Location]:
     result =[]
@@ -29,26 +29,32 @@ def get_low_points(height_map:List[List[int]]) -> List[Location]:
                 result.append(Location(value, row_index, column_index))
     return result
 
-def get_conneted_higher_points(loc: Location, height_map: List[List[int]]) -> List[Location]:
+def get_connected_higher_points(loc: Location, height_map: List[List[int]]) -> List[Location]:
     up = Location(height_map[loc.row -1][loc.column], loc.row -1, loc.column) if loc.row > 0 else None
     right= Location(height_map[loc.row][loc.column +1], loc.row, loc.column +1) if loc.column +1 < len(height_map[0]) else None
     left= Location(height_map[loc.row][loc.column -1], loc.row, loc.column -1) if loc.column > 0 else None
     down = Location(height_map[loc.row+1][loc.column], loc.row+1, loc.column) if loc.row+1 < len(height_map) else None
-    locations:List[Location] = [x for x in [up,down,left,right] if x != None]
-    return [x for x in locations if x.value >= loc.value and x.value != 9]
+    locations:List[Location] = [x for x in [up,down,left,right] if x != None and x.value != 9]
+    return [x for x in locations if x.value > loc.value]
 
 
 points = get_low_points(map) #512
 
 
-def get_pool(loc: Location, height_map: List[List[int]]) -> List[Location]:
-    result = []
-    for x in get_conneted_higher_points(loc, height_map):
-        result += get_pool(x, height_map)
+def get_pool(loc: Location, height_map: List[List[int]]) -> Set[Location]:
+    result = set([loc])
+    for x in get_connected_higher_points(loc, height_map):
+        [result.add(y) for y in get_pool(x, height_map) ]
     return result
 
-print(get_pool(points[1], map))
-print(get_conneted_higher_points(Location(value=1, row=0, column=8), map))
+
+res = []
+for point in points:
+    x = get_pool(point, map)
+    res.append(len(x))
+
+greatest = sorted(res)[-3:]
+print(greatest[0]* greatest[1]* greatest[2])
 
 
 
